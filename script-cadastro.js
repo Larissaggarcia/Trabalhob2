4f1f56372cc9026f85c703607930b91201bd34fb
Larissa Garcia <larissaggarcia@gmail.com>
2020-11-27 01:08:57 -0300

    ultimas alteracoes antes do envio

--- script-cadastro.js
+++ script-cadastro.js
@@ -5,13 +5,16 @@ function cadastro() {
 function addItem() {
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
+    const cpf = document.getElementById("cpf").value;
     const senha = document.getElementById("senha").value;
 
     const nomeobj = JSON.stringify({nome});
     const emailobj = JSON.stringify({email});
-    const senhaobj = JSON.stringify({senha})
+    const cpfobj = JSON.stringify({cpf});
+    const senhaobj = JSON.stringify({senha});
 
     sessionStorage.setItem('nome', nomeobj);
     sessionStorage.setItem('email', emailobj);
+    sessionStorage.setItem('cpf', cpfobj);
     sessionStorage.setItem('senha', senhaobj);
 }
--- script-cliente.js
+++ script-cliente.js
@@ -6,10 +6,12 @@ function renderItem() {
     
     var nomeUser = sessionStorage.getItem("nome").split('{"nome":"').join('');
     var emailUser = sessionStorage.getItem("email").split('{"email":"').join('');
+    var cpfUser = sessionStorage.getItem("cpf").split('{"cpf":"').join('');
     var senhaUser = sessionStorage.getItem("senha").split('{"senha":"').join('');
 
     
     document.getElementById("nome").innerHTML = `Nome: `+nomeUser.split('"}').join('');
     document.getElementById("email").innerHTML = `Email: `+emailUser.split('"}').join('');
+    document.getElementById("cpf").innerHTML = `cpf: `+cpfUser.split('"}').join('');
     document.getElementById("senha").innerHTML = `Senha: `+senhaUser.split('"}').join('');
 }
